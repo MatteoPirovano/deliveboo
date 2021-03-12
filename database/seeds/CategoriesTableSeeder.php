6e1f21fa79227443e2ff5fae090a505ed4ec3129
Vincenzo <enzo.antignani@gmail.com>
2021-03-12 16:38:46 +0100

    migration complete

---
+++ database/seeds/CategoriesTableSeeder.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Database\Seeder;
+/* use App\Category; */
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    /* $categories = [
+      [
+        "name" => "italiano",
+        "img" => "asdfasdfasdf"
+      ],
+      [
+        "name" => "messicano",
+        "img" => "asdfasdfasdf"
+      ],
+      [
+        "name" => "cinese",
+        "img" => "asdfasdfasdf"
+      ],
+      [
+        "name" => "giapponese",
+        "img" => "asdfasdfasdf"
+      ],
+      [
+        "name" =>  "indiano",
+        "img" => "asdfasdfasdf"
+      ],
+      [
+        "name" => "carne",
+        "img" => "asdfasdfasdf"
+      ],
+      [
+        "name" =>   "pesce",
+        "img" => "asdfasdfasdf"
+      ],
+      [
+        "name" => "vegetariano",
+        "img" => "asdfasdfasdf"
+      ],
+      [
+        "name" => "pizza",
+        "img" => "asdfasdfasdf"
+      ],
+      [
+        "name" => "fast-food",
+        "img" => "asdfasdfasdf"
+      ],
+
+    ]; */
+    }
+}
