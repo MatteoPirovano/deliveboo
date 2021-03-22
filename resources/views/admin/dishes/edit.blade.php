3beb1c6101ee73c7f74e4d01fa0d16e8a86f5e91
Marian <marian.corlade@outlook.it>
2021-03-22 13:11:19 +0100

    min=0

--- resources/views/admin/dishes/edit.blade.php
+++ resources/views/admin/dishes/edit.blade.php
@@ -51,7 +51,7 @@
 
         <div class="form-group">
             <label for="price" class="form-label">Prezzo</label>
-            <input type="number" step="0.01" class="form-control @error('price') is-invalid @enderror" name="price" id="price" value="{{$dish->price}}">
+            <input type="number" min="0" step="0.01" class="form-control @error('price') is-invalid @enderror" name="price" id="price" value="{{$dish->price}}">
         </div>
         <div>Visibilità</div>
         <div class="form-check">
