500aa693bd2021f1c3ceb1c7d8978a4d3096bbb4
Nicola <nicola.porta00@gmail.com>
2021-03-24 13:06:04 +0100

    Braintree Risolto

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -18,118 +18,83 @@
     </div>
     @endif
         
-    <div id="app">
-        <div class="order-pay-container container">
+      {{-- <div class="order-pay-container container">
           
-          <div class="cont-marg-left">
-            <div id="order-summary">
-            <div class="content pay-box">
-    </div>
+        <div class="cont-marg-left">
+          <div id="order-summary">
+            <div class="content pay-box"> --}}
+              
+              {{-- <div id="app"> --}}
               <form class="container" method="post" id="payment-form" action="{{url('/checkout')}}">
                 @csrf
                 @method('post')
                 
                 <section>
-                          {{-- <div class="order-summary-box">
-                            <div class="separate-summary">
-                              <h1>
-                                Riepilogo dell'ordine:
-                              </h1>
-                            </div>
-                            <div class="separate-summary input-group-prepend" v-for="(dish,index) in order">
-                                
-                                <input type="text" class="form-control" :name="'dish_name['+index+']'" :value="dish.name" readonly>
-                              
-                                
-                                <input type="text" class="form-control" :name="'dish_count['+index+']'" :value="dish.count" readonly>
-                              
-                                
-                                <input type="text" class="form-control" :name="'dish_price['+index+']'" :value="dish.price" readonly>
-                              
-                            </div>
-                            
-                            <div class="separate-summary">
-                                <input type="text" class="form-control" name="price" :value="total" readonly>
-                            </div>
-                          </div> --}}
-                          <div class="form-group">
-                            <label class="form-label" for="name">Nome</label>
-                            <input class="form-control" type="text" id="name" name="client_name" value="{{old('client_name')}}" placeholder="Inserisci nome">
-                          </div>
-                          <div class="form-group">
-                            <label class="form-label" for="surname">Cognome</label>
-                            <input class="form-control" id="surname" type="text" name="client_surname" value="{{old('client_surname')}}" placeholder="Inserisci cognome">
-                          </div>
-                          <div class="form-group">
-                            <label class="form-label" for="email">Email</label>
-                            <input class="form-control" type="email" id="email" name="client_mail" value="{{old('client_mail')}}" placeholder="Inserisci email">
-                          </div>
-                          <div class="form-group">
-                            <label class="form-label" for="address">Indirizzo</label>
-                            <input class="form-control" type="text" id="address" name="client_address" value="{{old('client_address')}}" placeholder="Inserisci indirizzo">
-                          </div>
-                          <div class="form-group">
-                            <label class="form-label" for="order_date">Data Ordine</label>
-                            <input class="form-control" type="datetime-local" id="order_date" name="order_date" value="{{old('order_date')}}" placeholder="Inserisci la data di ordinazione">
-                          </div>
-                          <div class="form-group">
-                            <label for="amount">
-                              <span class="input-label">Amount</span>
-                              <div class="input-wrapper amount-wrapper">
-                                <input id="amount" name="amount" type="tel" min="1" placeholder="amount" value="10">
-                              </div>
-                            </label>
-                          </div>
-                          <div class="bt-drop-in-wrapper">
-                            <div id="bt-dropin"></div>
-                          </div>
-                    </section>
-                        
-                        
-                        
-                        <input type="hidden" id="nonce" name="payment_method_nonce" type="text" readonly />
-                        <button id="pay-button" class="button" type="submit" v-on:click="deleteOrder()"><span>Effettua il Pagamento</span></button>
-              </form>
-                    {{-- </div> --}}
-                      
-                    
-        
-                      
-                    <script src="{{asset('js/payment.js')}}"></script>
-                      <script>
-                        var form = document.querySelector('#payment-form');
-                        var client_token ="{{$token}}";
-                        braintree.dropin.create({
-                          authorization: client_token,
-                          selector: '#bt-dropin',
-                          // paypal: {
-                          //   flow: 'vault'
-                          // }
-                        }, function (createErr, instance) {
-                          if (createErr) {
-                            console.log('Create Error', createErr);
-                            return;
-                          }
-                          form.addEventListener('submit', function (event) {
-                            event.preventDefault();
-                            instance.requestPaymentMethod(function (err, payload) {
-                              if (err) {
-                                console.log('Request Payment Method Error', err);
-                                return;
-                              }
-                              document.querySelector('#nonce').value = payload.nonce;
-                              form.submit();
-                            });
-                          });
-                        });
-                      </script>
-                      
-        
+                    <div class="form-group">
+                      <label class="form-label" for="name">Nome</label>
+                      <input class="form-control" type="text" id="name" name="client_name" value="{{old('client_name')}}" placeholder="Inserisci nome">
                     </div>
-        
-                  </div>
-            </div>
-          </div>
-        
+                    <div class="form-group">
+                      <label class="form-label" for="surname">Cognome</label>
+                      <input class="form-control" id="surname" type="text" name="client_surname" value="{{old('client_surname')}}" placeholder="Inserisci cognome">
+                    </div>
+                    <div class="form-group">
+                      <label class="form-label" for="email">Email</label>
+                      <input class="form-control" type="email" id="email" name="client_mail" value="{{old('client_mail')}}" placeholder="Inserisci email">
+                    </div>
+                    <div class="form-group">
+                      <label class="form-label" for="address">Indirizzo</label>
+                      <input class="form-control" type="text" id="address" name="client_address" value="{{old('client_address')}}" placeholder="Inserisci indirizzo">
+                    </div>
+                    <div class="form-group">
+                      <label class="form-label" for="order_date">Data Ordine</label>
+                      <input class="form-control" type="datetime-local" id="order_date" name="order_date" value="{{old('order_date')}}" placeholder="Inserisci la data di ordinazione">
+                    </div>
+                    <div class="form-group">
+                      <label for="amount">
+                        <span class="input-label">Amount</span>
+                        <div class="input-wrapper amount-wrapper">
+                          <input id="amount" name="amount" type="tel" min="1" placeholder="amount" value="10">
+                        </div>
+                      </label>
+                    </div>
+                    <div class="bt-drop-in-wrapper">
+                      <div id="bt-dropin"></div>
+                    </div>
+                    <input type="hidden" id="nonce" name="payment_method_nonce" type="text" readonly />
+                        <button id="pay-button" class="button" type="submit" v-on:click="deleteOrder()">
+                          <span>Effettua il Pagamento</span>
+                        </button>
+                </section> 
+              </form>
+      <script src="{{asset('js/payment.js')}}"></script>
+      <script>
+        var form = document.querySelector('#payment-form');
+        var client_token ="{{$token}}";
+        braintree.dropin.create({
+          authorization: client_token,
+          selector: '#bt-dropin',
+          // paypal: {
+          //   flow: 'vault'
+          // }
+        }, function (createErr, instance) {
+          if (createErr) {
+            console.log('Create Error', createErr);
+            return;
+          }
+          form.addEventListener('submit', function (event) {
+            event.preventDefault();
+            instance.requestPaymentMethod(function (err, payload) {
+              if (err) {
+                console.log('Request Payment Method Error', err);
+                return;
+              }
+              document.querySelector('#nonce').value = payload.nonce;
+              form.submit();
+            });
+          });
+        });
+      </script>
+          
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,89 @@ use Illuminate\Support\Str;
 |
 */
 
+Route::post('/checkout', function(Request $request) {
+  $gateway = new Braintree\Gateway([
+    'environment' => 'sandbox',
+    'merchantId' => '4szxkzqjrjntv2pj',
+    'publicKey' => 'f7cmspf5s5779bpk',
+    'privateKey' => '5d8ada5bd7f4a635c540fc7596f285f8'
+  ]);
+  $data = $request->all();
+    dd($data);
+        $request->validate([
+            'client_name'=> 'required|max:100',
+            'client_surname'=> 'required|max:100',
+            'client_address'=> 'required|max:100',
+            'client_mail'=> 'required|max:100|email:rfc,dns',
+            'order_date'=> 'required'
+        ]);
 
+        $order = new Order();
+        $order->fill($data);
+        $order->status="accepted";
+        $order_result = $order->save();
+
+        foreach($data['dish_name'] as $key => $slug) {
+            $slug = Str::slug($slug, '-');
+            $dishes[$key] = Dish::where('slug', $slug)->first();
+            if($order_result) {
+                if(!empty($dishes[$key])) {
+                    $dishes[$key]->orders()->attach($order->id);
+                }
+            }
+        }
+        
+
+  $amount = $request->amount;
+  $nonce = $request->payment_method_nonce;
+  
+  $result = $gateway->transaction()->sale([
+      'amount' => $amount,
+      'paymentMethodNonce' => $nonce,
+      'options' => [
+          'submitForSettlement' => true
+      ]
+  ]);
+  
+  if ($result->success) {
+      $transaction = $result->transaction;
+      // header("Location: " . $baseUrl . "transaction.php?id=" . $transaction->id);
+      return redirect()
+        ->route('payment_result', compact('data'))
+        ->with('message', 'ok');
+  } else {
+      $errorString = "";
+  
+      foreach($result->errors->deepAll() as $error) {
+          $errorString .= 'Error: ' . $error->code . ": " . $error->message . "\n";
+      }
+  
+      // $_SESSION["errors"] = $errorString;
+      // header("Location: " . $baseUrl . "index.php");
+      return back()->withErrors('ERROR');
+  }
+})->name('checkout');
+
+Route::get('/payment', function() {
+  // $gateway = new Braintree\Gateway([
+  //   'environment' => config('services.braintree.environment'),
+  //   'merchantId' => config('services.braintree.merchantId'),
+  //   'publicKey' => config('services.braintree.publicKey'),
+  //   'privateKey' => config('services.braintree.privateKey')
+  // ]);
+  $gateway = new Braintree\Gateway([
+    'environment' => 'sandbox',
+    'merchantId' => '4szxkzqjrjntv2pj',
+    'publicKey' => 'f7cmspf5s5779bpk',
+    'privateKey' => '5d8ada5bd7f4a635c540fc7596f285f8'
+  ]);
+  $token = $gateway->ClientToken()->generate();
+  return view('payment', ['token' => $token]);
+})->name('payment');
+
+Route::get('/payment_result', function() {
+  return view('guest.prova');
+})->name('payment_result');
 // Route::get('/payments', 'PaymentsController@pay')->name('payments');
 
 Route::get('/', function () {
@@ -48,92 +130,12 @@ Route::prefix('admin')       // prefisso delle rotte
     }
   );
 
+ 
+
   // Route::post('/prova', 'PaymentsController@prova')->name('prova');
 
   // Route::post('/result', 'PaymentsController@result')->name('result');
 
-  Route::get('/payment_result', function() {
-    return view('guest.prova');
-  })->name('payment_result');
 
-  Route::get('/payment', function() {
-    // $gateway = new Braintree\Gateway([
-    //   'environment' => config('services.braintree.environment'),
-    //   'merchantId' => config('services.braintree.merchantId'),
-    //   'publicKey' => config('services.braintree.publicKey'),
-    //   'privateKey' => config('services.braintree.privateKey')
-    // ]);
-    $gateway = new Braintree\Gateway([
-      'environment' => 'sandbox',
-      'merchantId' => '4szxkzqjrjntv2pj',
-      'publicKey' => 'f7cmspf5s5779bpk',
-      'privateKey' => '5d8ada5bd7f4a635c540fc7596f285f8'
-    ]);
-    $restaurant = Restaurant::all();
-    $token = $gateway->ClientToken()->generate();
-    return view('payment', ['token' => $token]);
-  })->name('payment');
+
   
-  Route::post('/checkout', function(Request $request) {
-    $gateway = new Braintree\Gateway([
-      'environment' => 'sandbox',
-      'merchantId' => '4szxkzqjrjntv2pj',
-      'publicKey' => 'f7cmspf5s5779bpk',
-      'privateKey' => '5d8ada5bd7f4a635c540fc7596f285f8'
-    ]);
-    $data = $request->all();
-      dd($data);
-          $request->validate([
-              'client_name'=> 'required|max:100',
-              'client_surname'=> 'required|max:100',
-              'client_address'=> 'required|max:100',
-              'client_mail'=> 'required|max:100|email:rfc,dns',
-              'order_date'=> 'required'
-          ]);
-  
-          $order = new Order();
-          $order->fill($data);
-          $order->status="accepted";
-          $order_result = $order->save();
-  
-  
-          foreach($data['dish_name'] as $key => $slug) {
-              $slug = Str::slug($slug, '-');
-              $dishes[$key] = Dish::where('slug', $slug)->first();
-              if($order_result) {
-                  if(!empty($dishes[$key])) {
-                      $dishes[$key]->orders()->attach($order->id);
-                  }
-              }
-          }
-          
-  
-    $amount = $request->amount;
-    $nonce = $request->payment_method_nonce;
-    
-    $result = $gateway->transaction()->sale([
-        'amount' => $amount,
-        'paymentMethodNonce' => $nonce,
-        'options' => [
-            'submitForSettlement' => true
-        ]
-    ]);
-    
-    if ($result->success) {
-        $transaction = $result->transaction;
-        // header("Location: " . $baseUrl . "transaction.php?id=" . $transaction->id);
-        return redirect()
-          ->route('payment_result', compact('data'))
-          ->with('message', 'ok');
-    } else {
-        $errorString = "";
-    
-        foreach($result->errors->deepAll() as $error) {
-            $errorString .= 'Error: ' . $error->code . ": " . $error->message . "\n";
-        }
-    
-        // $_SESSION["errors"] = $errorString;
-        // header("Location: " . $baseUrl . "index.php");
-        return back()->withErrors('ERROR');
-    }
-  })->name('checkout');
