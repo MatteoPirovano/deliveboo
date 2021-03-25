7d8206ee05c4b5251dd905a419a6ea5823c9d70a
Marian <marian.corlade@outlook.it>
2021-03-25 10:10:20 +0100

    pagina pagamenti init

--- resources/views/guest/prova.blade.php
+++ resources/views/guest/prova.blade.php
@@ -1,8 +1,18 @@
-
-{{-- <div id="app">
-    @dd($data)
+<!DOCTYPE html>
+<html lang="en">
+    <link href="{{ asset('css/_paymentSuccess.css') }}" rel="stylesheet">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
     
-</div> --}}
+    <script src="{{asset('js/checkout.js')}}"></script>
+</body>
+</html>
 
-CIAO
-<script src="{{asset('js/checkout.js')}}"></script>
+
+
+
