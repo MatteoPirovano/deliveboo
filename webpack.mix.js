9ec5d5e562d78d7f79a3002e84ca8d63a923f088
Nicola <nicola.porta00@gmail.com>
2021-03-25 13:13:25 +0100

    webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -18,11 +18,8 @@ mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .sass('resources/sass/app2.scss', 'public/css')
     .sass('resources/sass/menu.scss', 'public/css')
-<<<<<<< HEAD
     .sass('resources/sass/paymentSuccess.scss', 'public/css')
-=======
     .sass('resources/sass/payment.scss', 'public/css')
->>>>>>> LayoutPagamento
     .vue({version:2});
     /* .options({
     processCssUrls: false // questa opzione fa in modo da non modificare i percorsi delle immagini
