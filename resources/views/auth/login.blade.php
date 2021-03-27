259ed2aabb9c53a683a7af2da74febbcd5ef36ed
Marian <marian.corlade@outlook.it>
2021-03-27 17:54:23 +0100

    login responsive 768

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div id="col_md_768_ms" class="col-md-8">
             <div class="card">
 
                 {{-- avatr in position absolute --}}
