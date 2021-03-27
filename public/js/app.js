d90951f8718e4ba49d797bf57614746561f4654a
Marian <marian.corlade@outlook.it>
2021-03-27 18:45:07 +0100

    correzione errori merge

--- public/js/app.js
+++ public/js/app.js
@@ -34569,9 +34569,12 @@ __webpack_require__.r(__webpack_exports__);
 /*!*********************************!*\
   !*** ./resources/sass/app.scss ***!
   \*********************************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nSassError: expected \"{\".\n    ╷\n618 │ >>>>>>> ResponsiveBackend\r\n    │                          ^\n    ╵\n  resources\\sass\\partials_backend\\_main.scss 618:26  @import\n  C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\resources\\sass\\app.scss 10:9                                         root stylesheet\n    at processResult (C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\webpack\\lib\\NormalModule.js:598:19)\n    at C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\webpack\\lib\\NormalModule.js:692:5\n    at C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\loader-runner\\lib\\LoaderRunner.js:399:11\n    at C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\loader-runner\\lib\\LoaderRunner.js:251:18\n    at context.callback (C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\loader-runner\\lib\\LoaderRunner.js:124:13)\n    at C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\sass-loader\\dist\\index.js:73:7\n    at Function.call$2 (C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\sass\\sass.dart.js:91729:16)\n    at _render_closure1.call$2 (C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\sass\\sass.dart.js:80373:12)\n    at _RootZone.runBinary$3$3 (C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\sass\\sass.dart.js:27269:18)\n    at _FutureListener.handleError$1 (C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\sass\\sass.dart.js:25797:19)\n    at _Future__propagateToListeners_handleError.call$0 (C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\sass\\sass.dart.js:26094:49)\n    at Object._Future__propagateToListeners (C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\sass\\sass.dart.js:4543:77)\n    at _Future._completeError$2 (C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\sass\\sass.dart.js:25927:9)\n    at _AsyncAwaitCompleter.completeError$2 (C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\sass\\sass.dart.js:25270:12)\n    at Object._asyncRethrow (C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\sass\\sass.dart.js:4292:17)\n    at C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\sass\\sass.dart.js:13233:20");
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
 
 /***/ }),
 
@@ -34579,9 +34582,12 @@ throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugi
 /*!**********************************!*\
   !*** ./resources/sass/app2.scss ***!
   \**********************************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nSassError: expected \"{\".\n    ╷\n195 │ >>>>>>> Stashed changes\r\n    │                        ^\n    ╵\n  resources\\sass\\partials_frontend\\_header.scss 195:24  @import\n  C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\resources\\sass\\app2.scss 4:9                                             root stylesheet\n    at processResult (C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\webpack\\lib\\NormalModule.js:598:19)\n    at C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\webpack\\lib\\NormalModule.js:692:5\n    at C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\loader-runner\\lib\\LoaderRunner.js:399:11\n    at C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\loader-runner\\lib\\LoaderRunner.js:251:18\n    at context.callback (C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\loader-runner\\lib\\LoaderRunner.js:124:13)\n    at C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\sass-loader\\dist\\index.js:73:7\n    at Function.call$2 (C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\sass\\sass.dart.js:91729:16)\n    at _render_closure1.call$2 (C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\sass\\sass.dart.js:80373:12)\n    at _RootZone.runBinary$3$3 (C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\sass\\sass.dart.js:27269:18)\n    at _FutureListener.handleError$1 (C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\sass\\sass.dart.js:25797:19)\n    at _Future__propagateToListeners_handleError.call$0 (C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\sass\\sass.dart.js:26094:49)\n    at Object._Future__propagateToListeners (C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\sass\\sass.dart.js:4543:77)\n    at _Future._completeError$2 (C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\sass\\sass.dart.js:25927:9)\n    at _AsyncAwaitCompleter.completeError$2 (C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\sass\\sass.dart.js:25270:12)\n    at Object._asyncRethrow (C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\sass\\sass.dart.js:4292:17)\n    at C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\sass\\sass.dart.js:13233:20");
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
 
 /***/ }),
 
@@ -34589,9 +34595,12 @@ throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugi
 /*!**********************************!*\
   !*** ./resources/sass/menu.scss ***!
   \**********************************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nSassError: expected \"{\".\n    ╷\n195 │ >>>>>>> Stashed changes\r\n    │                        ^\n    ╵\n  resources\\sass\\partials_frontend\\_header.scss 195:24  @import\n  C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\resources\\sass\\menu.scss 3:9                                             root stylesheet\n    at processResult (C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\webpack\\lib\\NormalModule.js:598:19)\n    at C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\webpack\\lib\\NormalModule.js:692:5\n    at C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\loader-runner\\lib\\LoaderRunner.js:399:11\n    at C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\loader-runner\\lib\\LoaderRunner.js:251:18\n    at context.callback (C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\loader-runner\\lib\\LoaderRunner.js:124:13)\n    at C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\sass-loader\\dist\\index.js:73:7\n    at Function.call$2 (C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\sass\\sass.dart.js:91729:16)\n    at _render_closure1.call$2 (C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\sass\\sass.dart.js:80373:12)\n    at _RootZone.runBinary$3$3 (C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\sass\\sass.dart.js:27269:18)\n    at _FutureListener.handleError$1 (C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\sass\\sass.dart.js:25797:19)\n    at _Future__propagateToListeners_handleError.call$0 (C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\sass\\sass.dart.js:26094:49)\n    at Object._Future__propagateToListeners (C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\sass\\sass.dart.js:4543:77)\n    at _Future._completeError$2 (C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\sass\\sass.dart.js:25927:9)\n    at _AsyncAwaitCompleter.completeError$2 (C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\sass\\sass.dart.js:25270:12)\n    at Object._asyncRethrow (C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\sass\\sass.dart.js:4292:17)\n    at C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\sass\\sass.dart.js:13233:20");
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
 
 /***/ }),
 
@@ -34599,9 +34608,12 @@ throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugi
 /*!********************************************!*\
   !*** ./resources/sass/paymentSuccess.scss ***!
   \********************************************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nSassError: expected \"{\".\n    ╷\n195 │ >>>>>>> Stashed changes\r\n    │                        ^\n    ╵\n  resources\\sass\\partials_frontend\\_header.scss 195:24  @import\n  C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\resources\\sass\\paymentSuccess.scss 3:9                                             root stylesheet\n    at processResult (C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\webpack\\lib\\NormalModule.js:598:19)\n    at C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\webpack\\lib\\NormalModule.js:692:5\n    at C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\loader-runner\\lib\\LoaderRunner.js:399:11\n    at C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\loader-runner\\lib\\LoaderRunner.js:251:18\n    at context.callback (C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\loader-runner\\lib\\LoaderRunner.js:124:13)\n    at C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\sass-loader\\dist\\index.js:73:7\n    at Function.call$2 (C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\sass\\sass.dart.js:91729:16)\n    at _render_closure1.call$2 (C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\sass\\sass.dart.js:80373:12)\n    at _RootZone.runBinary$3$3 (C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\sass\\sass.dart.js:27269:18)\n    at _FutureListener.handleError$1 (C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\sass\\sass.dart.js:25797:19)\n    at _Future__propagateToListeners_handleError.call$0 (C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\sass\\sass.dart.js:26094:49)\n    at Object._Future__propagateToListeners (C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\sass\\sass.dart.js:4543:77)\n    at _Future._completeError$2 (C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\sass\\sass.dart.js:25927:9)\n    at _AsyncAwaitCompleter.completeError$2 (C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\sass\\sass.dart.js:25270:12)\n    at Object._asyncRethrow (C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\sass\\sass.dart.js:4292:17)\n    at C:\\Users\\ca2b3\\Desktop\\Boolean\\Esercitazioni\\deliveboo\\node_modules\\sass\\sass.dart.js:13233:20");
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
 
 /***/ }),
 
@@ -49827,12 +49839,12 @@ Vue.compile = compileToFunctions;
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module depends on other loaded chunks and execution need to be delayed
-/******/ 	__webpack_require__.O(undefined, ["css/payment"], () => (__webpack_require__("./resources/js/app.js")))
-/******/ 	__webpack_require__.O(undefined, ["css/payment"], () => (__webpack_require__("./resources/sass/app.scss")))
-/******/ 	__webpack_require__.O(undefined, ["css/payment"], () => (__webpack_require__("./resources/sass/app2.scss")))
-/******/ 	__webpack_require__.O(undefined, ["css/payment"], () => (__webpack_require__("./resources/sass/menu.scss")))
-/******/ 	__webpack_require__.O(undefined, ["css/payment"], () => (__webpack_require__("./resources/sass/paymentSuccess.scss")))
-/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["css/payment"], () => (__webpack_require__("./resources/sass/payment.scss")))
+/******/ 	__webpack_require__.O(undefined, ["css/app","css/paymentSuccess","css/menu","css/app2","css/payment"], () => (__webpack_require__("./resources/js/app.js")))
+/******/ 	__webpack_require__.O(undefined, ["css/app","css/paymentSuccess","css/menu","css/app2","css/payment"], () => (__webpack_require__("./resources/sass/app.scss")))
+/******/ 	__webpack_require__.O(undefined, ["css/app","css/paymentSuccess","css/menu","css/app2","css/payment"], () => (__webpack_require__("./resources/sass/app2.scss")))
+/******/ 	__webpack_require__.O(undefined, ["css/app","css/paymentSuccess","css/menu","css/app2","css/payment"], () => (__webpack_require__("./resources/sass/menu.scss")))
+/******/ 	__webpack_require__.O(undefined, ["css/app","css/paymentSuccess","css/menu","css/app2","css/payment"], () => (__webpack_require__("./resources/sass/paymentSuccess.scss")))
+/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["css/app","css/paymentSuccess","css/menu","css/app2","css/payment"], () => (__webpack_require__("./resources/sass/payment.scss")))
 /******/ 	__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
 /******/ 	
 /******/ })()
